571b8ebc680329462f493fff534683567faf2832
Anushka  ✨ <222953857+Anushka-345@users.noreply.github.com>
2026-03-26 10:22:29 +0530

    Time: 0 ms (100%), Space: 11.1 MB (71.19%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -12,43 +12,36 @@ class Solution {
 public:
      ListNode* reverseBetween(ListNode* head, int left, int right) {
     if (!head || left == right || !head->next) return head;
+    ListNode* curr = head ;
+    ListNode* prev = NULL;
+    int i = 1 ;
 
-    ListNode *curr = head;
-    ListNode *prev = NULL;
-    int i = 1;
-
-    // Step 1: Move to 'left'
-    while (curr != NULL && i != left) {
+    while(curr && i != left){
         prev = curr;
         curr = curr->next;
         i++;
     }
-
-    ListNode *StartPoint = prev;  // node before left
-    ListNode *start = curr;       // first node of sublist
-
-    prev = NULL;
-
-    // Step 2: Reverse till right
-    while (curr != NULL && i != right + 1) {
-        ListNode *front = curr->next;
-        curr->next = prev;
+    ListNode* StartNode = prev ;
+    ListNode* start = curr;
+     prev = NULL;
+    while(curr &&  i != right+1){
+        ListNode*  front = curr->next;
+        curr->next = prev ;
         prev = curr;
         curr = front;
         i++;
     }
-
-    // Step 3: Connect tail of reversed part
     start->next = curr;
-
-    // Step 4: Connect head part
-    if (StartPoint != NULL) {
-        StartPoint->next = prev;
-    } else {
-        return prev; // new head if left == 1
+    if(StartNode){
+        StartNode->next = prev ;
     }
-
-    return head;
-
-}
+    else{
+        return prev;
+    }
+     return head;
+     }
 };
+
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
